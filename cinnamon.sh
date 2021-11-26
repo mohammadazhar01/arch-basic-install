ea7bbcbe77c691ad23f0fea6a03fdcbca0b94ae4
Azhar <mdazzyy@gmail.com>
2021-11-26 18:48:42 +0530

    cinnamon installation script

---
+++ cinnamon.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo pacman -Syyy
+sudo pacman -S xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings lightdm-slick-greeter blueberry cinnamon metacity gnome-terminal gnome-keyring gnome-screenshot firefox system-config-printer
